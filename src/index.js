4bb0e9abf0a6dd068bfd5acacffd4e3207868a81
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-09-01 15:40:26 +0900

    session, sessionStore

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import express from "express";
 import bodyParser from "body-parser";
 import routes from "./routes";
-const app = express();
+import mysql from "./db/mysql";
+import session from "express-session";
+var MySQLStore = require("express-mysql-session")(session);
+import storeOption from "./options/mysqlstoreopts";
+import dotenv from "dotenv";
 
-let port = 8080;
+dotenv.config();
+
+const app = express();
+let port = 8081;
+
+const sessionStore = new MySQLStore(storeOption);
+
 //static file, folder
 
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    key: process.env.SESSION_KEY,
+    secret: process.env.SESSION_SECRETKEY,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
 //router
 app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  const sql = "SHOW TABLES";
+  mysql.query(sql, (err, results, fields) => {
+    if (err) console.log(err);
+    console.log(results);
+  });
+  req.session.test = 1;
+  res.send("Hello world" + req.session.test);
+});
 
 app.listen(port, () => {
   console.log("what-to-see-server listening at ", port);
